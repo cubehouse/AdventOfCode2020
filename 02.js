8b1d421abd8e5fc9ef758220efa9ea192bc97864
cubehouse <me@jamie.holdings>
2020-12-03 10:34:33 +0000

    [~] Tidy up comments

--- 02.js
+++ 02.js
@@ -6,7 +6,7 @@ async function Run() {
     const reg = /(\d+)-(\d+)\s(\w):\s(\w+)/;
     // map all inputs to the regex match result
     const pws = input.map((x) => reg.exec(x)).filter((x) => !!x).map((x) => {
-        // format the regex captures nearly for easier access later
+        // format the regex captures neatly for easier access later
         return {
             password: x[4],
             min: Number(x[1]),
@@ -18,6 +18,7 @@ async function Run() {
     // filter out all pws that don't match rule
     await Advent.Submit(pws.filter((x) => {
         // quick trick to count characters in JS, split by character and count array size
+        //  quick in terms of time-to-write, this whole 2 parter runs in < 100ms, so I'm not worried right now
         const num = x.password.split(x.char).length - 1;
         return num >= x.min && num <= x.max;
     }).length);
