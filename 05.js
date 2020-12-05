da7b50c8088693c29dec18cf0126fe01ccbcf4ec
cubehouse <me@jamie.holdings>
2020-12-05 11:56:55 +0000

    [+] Day 5 part 2

--- 05.js
+++ 05.js
@@ -8,7 +8,16 @@ async function Run() {
 
     // find max ID in the array
     await Advent.Submit(Math.max.apply(this, IDs));
-    
-    // await Advent.Submit(null, 2);
+
+    // sort our IDs so we can find the missing ticket
+    IDs.sort((a, b) => a - b);
+    const mySeat = IDs.find((x, idx, arr) => {
+        // find the element in the array where the next element is +2
+        //  (i.e, there is exactly 1 missing sequential ticket)
+        //  this is the ticket *before* ours, so + 1
+        return (arr[idx + 1] === x + 2);
+    }) + 1;
+
+    await Advent.Submit(mySeat, 2);
 }
 Run();
