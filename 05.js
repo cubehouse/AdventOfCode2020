1dadc91aad8cf307c4b977d4a1c5e90df1dff6f2
cubehouse <me@jamie.holdings>
2020-12-05 11:51:58 +0000

    [+] Day 5 part 1

--- 05.js
+++ 05.js
@@ -3,7 +3,12 @@ const Advent = new (require('./index.js'))(5, 2020);
 async function Run() {
     const input = await Advent.GetInput();
 
-    // await Advent.Submit(null);
+    // FBLR are just binary numbers, convert to binary and parse into decimal
+    const IDs = input.map((x) => parseInt(x.replace(/[FL]/g, '0').replace(/[BR]/g, '1'), 2));
+
+    // find max ID in the array
+    await Advent.Submit(Math.max.apply(this, IDs));
+    
     // await Advent.Submit(null, 2);
 }
 Run();
