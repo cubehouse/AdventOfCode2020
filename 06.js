89a93ca975bf96d4a6753943afd47246d40f5d0a
cubehouse <me@jamie.holdings>
2020-12-06 10:50:09 +0000

    [+] Day 6

--- 06.js
+++ 06.js
@@ -3,7 +3,33 @@ const Advent = new (require('./index.js'))(6, 2020);
 async function Run() {
     const input = await Advent.GetInput();
 
-    // await Advent.Submit(null);
-    // await Advent.Submit(null, 2);
+    const groups = input.reduce((p, x) => {
+        if (x === '') {
+            p.push({forms: []});
+        } else {
+            p[p.length-1].forms.push(x);
+        }
+        return p;
+    }, [{forms: []}]);
+
+    groups.forEach((group) => {
+        group.questions = Array.from(new Set(group.forms.join('').split('')));
+        group.questions.sort();
+    });
+
+    await Advent.Submit(groups.reduce((p, x) => {
+        return p + x.questions.length;
+    }, 0));
+
+    groups.forEach((group) => {
+        group.allAns = group.questions.filter((x) => {
+            return !group.forms.find((n) => {
+                return n.indexOf(x) < 0;
+            });
+        });
+    });
+    await Advent.Submit(groups.reduce((p, x) => {
+        return p + x.allAns.length;
+    }, 0), 2);
 }
 Run();
