397866c4333e479a6f2a60cdebcb67048529a895
Jamie Holding <me@jamie.holdings>
2020-12-09 08:28:44 +0000

    [+] Day 9 part 1

--- 09.js
+++ 09.js
@@ -1,9 +1,44 @@
 const Advent = new (require('./index.js'))(9, 2020);
 
 async function Run() {
-    const input = await Advent.GetInput();
+    const input = (await Advent.GetInput()).map(Number);
+    const input2 = `35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576`.split(/\n/g).map(Number);
 
-    // await Advent.Submit(null);
+    const findInvalidEntry = (arr, preambleSize) => {
+        for(let idx=preambleSize; idx<arr.length; idx++) {
+            const nums = arr.slice(idx-preambleSize, idx);
+            const val = arr[idx];
+            const searches = nums.map((x) => val - x).filter((x, idx) => x !== nums[idx] && x > 0).filter((x, arr) => {
+                return nums.indexOf(x) >= 0;
+            });
+            if (searches.length === 0) {
+                return val;
+            }
+        }
+        return undefined;
+    };
+
+    const invalidNum = findInvalidEntry(input, 25);
+    await Advent.Submit(invalidNum);
     // await Advent.Submit(null, 2);
 }
 Run();
