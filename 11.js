f6013aa7248e0d15b678709ce345739fb77db00a
cubehouse <me@jamie.holdings>
2020-12-13 12:31:56 +0000

    [+] Day 11 part 2

--- 11.js
+++ 11.js
@@ -3,16 +3,6 @@ const Grid = require('./grid.js');
 
 async function Run() {
     const input = await Advent.GetInput();
-    const input2 = `L.LL.LL.LL
-LLLLLLL.LL
-L.L.L..L..
-LLLL.LL.LL
-L.LL.LL.LL
-L.LLLLL.LL
-..L.L.....
-LLLLLLLLLL
-L.LLLLLL.L
-L.LLLLL.LL`.split(/\n/g);
 
     const g = new Grid();
     input.forEach((line, y) => {
@@ -42,21 +32,21 @@ L.LLLLL.LL`.split(/\n/g);
             };
         })
     };
-    const occupiedNeighbours = (gridObj, x, y) => {
-        return getNeighbours(gridObj, x, y).filter((x) => x.val === '#');
+    const occupiedNeighbours = (gridObj, x, y, nFunc) => {
+        return nFunc(gridObj, x, y).filter((x) => x.val === '#');
     };
 
-    const step = (gridObj) => {
+    const step = (gridObj, nFunc, seatThreshold = 4) => {
         const writes = [];
         gridObj.ForEach((x, y, val) => {
             if (val === 'L') {
-                const nei = occupiedNeighbours(gridObj, x, y);
+                const nei = occupiedNeighbours(gridObj, x, y, nFunc);
                 if (nei.length === 0) {
                     writes.push([x, y, '#']);
                 }
             } else if (val === '#') {
-                const nei = occupiedNeighbours(gridObj, x, y);
-                if (nei.length >= 4) {
+                const nei = occupiedNeighbours(gridObj, x, y, nFunc);
+                if (nei.length >= seatThreshold) {
                     writes.push([x, y, 'L']);
                 }
             }
@@ -70,7 +60,7 @@ L.LLLLL.LL`.split(/\n/g);
 
     // step until the layout doesn't change
     while(true) {
-        step(g);
+        step(g, getNeighbours);
         if (!g._dirty) {
             break;
         }
@@ -81,6 +71,41 @@ L.LLLLL.LL`.split(/\n/g);
     }).filter((x) => x === '#');
 
     await Advent.Submit(count.length);
-    // await Advent.Submit(null, 2);
+
+    // part 2
+
+    const g2 = new Grid();
+    input.forEach((line, y) => {
+        line.split('').forEach((char, x) => {
+            g2.Write(x, y, char);
+        });
+    });
+
+    const getSeatsInEyeline = (gridObj, x, y) => {
+        return neighbours.map((dir) => {
+            return gridObj.Trace(x, y, dir[0], dir[1], (x, y, val) => {
+                // continue trace while we're hitting floor, otherwise stop
+                return val === '.';
+            });
+        }).filter((t) => t[2] !== undefined).map((x) => {
+            return {
+                x: x[0],
+                y: x[1],
+                val: x[2],
+            };
+        });
+    };
+
+    while(true) {
+        step(g2, getSeatsInEyeline, 5);
+        if (!g2._dirty) {
+            break;
+        }
+    }
+    const count2 = g2.Map((x, y, val) => {
+        return val;
+    }).filter((x) => x === '#');
+
+    await Advent.Submit(count2.length, 2);
 }
 Run();
