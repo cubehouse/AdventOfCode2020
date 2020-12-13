40041d99a61f7033c9804f1dcfb798b1138bacc5
cubehouse <me@jamie.holdings>
2020-12-13 13:14:49 +0000

    [+] Day 13 part 1

--- 13.js
+++ 13.js
@@ -3,7 +3,17 @@ const Advent = new (require('./index.js'))(13, 2020);
 async function Run() {
     const input = await Advent.GetInput();
 
-    // await Advent.Submit(null);
+    const start = Number(input[0]);
+    const buses = input[1].split(',').filter((x) => x !== 'x').map(Number);
+
+    const waitTime = buses.map((x) => {
+        // divide our arrival time by bus frequency, getting the remainer (%)
+        //  then remove this from the bus frequency to get the next arrival time
+        return x - (start % x);
+    });
+    const nextBus = waitTime.indexOf(Math.min(...waitTime));
+
+    await Advent.Submit(waitTime[nextBus] * buses[nextBus]);
     // await Advent.Submit(null, 2);
 }
 Run();
